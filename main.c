82b18c5ca0902dceb688c9d9e0d1c9c42b1d642c
alouane04 <ariahi@student.1337.ma>
2022-12-28 11:27:59 +0100

    ok

--- main.c
+++ main.c
@@ -3,19 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eel-moun <eel-moun@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 18:44:20 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/28 10:44:26 by eel-moun         ###   ########.fr       */
+/*   Updated: 2022/12/28 11:24:45 by ariahi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3D.h"
 
+int	check_error(t_input *input)
+{
+	int	i;
+	
+	i = 0;
+	
+}
+
 int main(int ac, char **av)
 {
-    
-    int fd = open("map.cub", O_RDWR);
+    t_input	input;
 
-    printf("%d\n", fd);
+	if (ac != 2)
+		return (ft_putendl_fd("Error", 2), 1);
+    int fd = open(av[1], O_RDWR);
+	if (!fd)
+		return (fd_putendl_fd("Error can't open map", 2), 1);
+	if (ft_strsep(&av[1], ))
+	if (check_error(&input))
+		return (1);
+
+	return (0);
 }
