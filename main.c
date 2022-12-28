76728c76f794f9c2ddd0e3776a173953d2186946
El amine El mountassir <eel-moun@e2r13p1.1337.ma>
2022-12-28 14:43:10 +0100

    getting main

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ariahi <ariahi@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/27 18:44:20 by ariahi            #+#    #+#             */
-/*   Updated: 2022/12/28 08:44:05 by ariahi           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "cube3D.h"
+
+t_list	*stock_input(int fd)
+{
+	t_list	*input;
+	char	*str;
+
+	input = NULL;
+	while(1)
+	{
+		str =get_next_line(fd);
+		if(*str == '\0')
+			break;
+		else if(!str)
+			return(free(input), NULL);
+		ft_lstadd_back(&input, ft_lstnew(str));
+		free(str);
+	}
+	return (free(str),input);
+}
+
+int main(int ac, char **av)
+{
+}
