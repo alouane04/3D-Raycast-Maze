fa0cf1e86bfd4ae344289d16698249797bdbf4ec
alouane04 <ariahi@student.1337.ma>
2022-12-28 14:38:09 +0100

    commit

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -3,23 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eel-moun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: eel-moun <eel-moun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 21:52:54 by eel-moun          #+#    #+#             */
-/*   Updated: 2022/03/31 21:52:59 by eel-moun         ###   ########.fr       */
+/*   Updated: 2022/12/28 12:09:24 by eel-moun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFFER_SIZE = 1024
 # include <stdio.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
 
-size_t	ft_strlen(const char *s);
-char	*ft_strchr(char *s, int c);
-char	*ft_strjoin(char *s, char *buff);
+size_t	ft_strlen_utils(const char *s);
+char	*ft_strchr_utils(char *s, int c);
+char	*ft_strjoin_utils(char *s, char *buff);
 char	*get_next_line(int fd);
 char	*get_the_line(char *buffer);
 char	*get_the_res(char *buffer);
