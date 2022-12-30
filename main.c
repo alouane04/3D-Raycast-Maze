0420054ade0d833eccf4976043f97711797b8cdb
alouane04 <ariahi@student.1337.ma>
2022-12-30 20:25:04 +0100

    sigfault need to be found

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int ac, char **av)
 {
 	t_input	input;
 	int		fd;
-	
+
 	if (ac != 2)
 		return (ft_putendl_fd("Error invalid argument", 2), 1);
 	fd = open(av[1], O_RDONLY);
@@ -15,7 +15,7 @@ int main(int ac, char **av)
 		return (ft_putendl_fd("Error invalid map name", 2), 1);
 	init_input(&input);
 	if (stock_input(fd, &input))
-		return(free_ip(&input), 1);
+		return(free_input(&input), 1);
 	printf("%d\n", input.c[0]);
 	return (0);
 }
--- parsing_stock.c
+++ parsing_stock.c
@@ -34,37 +34,104 @@ int	*stock_rgb(char *str)
 	return (ft_free(c_rgb), i_rgb);
 }
 
-int	check_if_empty(t_input *input)
+void	stock_lst(int fd, char *str, t_list *lst)
 {
-	if (!input->no)
-		return (1);
-	if (!input->so)
-		return (1);
-	if (!input->ea)
-		return (1);
-	if (!input->we)
-		return (1);
-	if (!input->f)
-		return (1);
-	if (!input->c)
-		return (1);
-	return (0);
+	char	*str1;
+
+	str1 = get_next_line(fd);
+
+	// write(2, str, 5);
+	// write(2, "\n", 1);
+	ft_lstadd_back(&lst, ft_lstnew(str));
+	// if (lst->content)
+	// 	write(2, lst->content, 5);
+	check_if_map(fd, str1, lst);
+}
+
+int	get_biggest_line(t_list *lst)
+{
+	int	i;
+	//char	*str;
+
+	//str = lst->content;
+	//if (!str)
+	//	write(2, "test\n", 5);
+	write(2, "9alwa\n", 7);
+	i = ft_strlen((char*)lst->content);
+	while (lst)
+	{
+		if (i < (int)ft_strlen((char*)lst->content))
+			i = ft_strlen(lst->content);
+		lst = lst->next;
+	}
+	return (i);
+}
+
+char	*stock_line(char const *s, int len)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	str = ft_calloc(len + 1, sizeof(char));
+	if (!str)
+		return (0);
+	while (s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	return (str);
+}
+
+void	stock_map(t_list *lst, t_input *input)
+{
+	int len;
+	int i;
+
+	i = 0;
+	input->map = ft_calloc(ft_lstsize(lst) + 1, sizeof(char *));
+	if (!input->map)
+		return ;
+	len = get_biggest_line(lst);
+	while (lst)
+	{
+		input->map[i] = stock_line(lst->content, len);
+		i++;
+		lst = lst->next;
+	}
 }
 
 int	stock_input(int fd, t_input *input)
 {
 	char	*str;
+	t_list	*lst;
 
+	lst = NULL;
 	while(1)
 	{
 		str = get_next_line(fd);
 		if (!str)
 			break;
+		//printf("%s\n", str);
 		if (check_line(str, input))
-			return (free(str), ft_putendl_fd("Error invalid map", 2), 1);
+		{
+			if (!check_if_map(fd, str, lst))
+			{
+				//write(2, "koo\n", 4);
+				//if(!lst->content)
+					//write(2, "ok\n", 3);
+				stock_map(lst, input);
+				break ;
+			}
+			else
+			{
+				return (free(str), ft_putendl_fd("Error invalid map", 2), 1);
+			}
+		}
 		free(str);
 	}
-	if (check_if_empty(input))
-		return (free(str), ft_putendl_fd("Error invalid map1", 2), 1);
+	if (check_if_empty(input) || str)
+		return (free(str), ft_putendl_fd("Error invalid map", 2), 1);
 	return (free(str), 0);
 }
